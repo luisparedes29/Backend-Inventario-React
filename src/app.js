102eaa5b8bb87b9bfd020cf5ddb3a81873c0504d
luisparedes29 <luisparedescrv@gmail.com>
2023-06-01 23:09:49 -0400

    añadido cors

--- src/app.js
+++ src/app.js
@@ -13,13 +13,14 @@ var app = express();
 
 // view engine setup
 
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+
 
 app.use('/', indexRouter);
 
